4f44acc0ea5f1eb368de2532a82990c32b3a9cc1
Van Truong <truongtran.lv2019@gmail.com>
2024-07-19 10:40:40 +0700

    update: using useCheckout save deliveryAddress

--- src/app/(guest)/checkout/delivery-address.tsx
+++ src/app/(guest)/checkout/delivery-address.tsx
@@ -10,19 +10,58 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export function DeliveryAddress() {
   const { user } = useUser();
-  const { setDeliveryAddress } = useCheckout();
-  const [address, setAddress] = useState<Address>();
+  const { deliveryAddress, setDeliveryAddress } = useCheckout();
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await userAddressApi.getDefaultAddress(user.email);
-      setAddress(res.payload.data);
-      setDeliveryAddress(res.payload.data.id);
+      setDeliveryAddress(res.payload.data);
     };
     fetchData();
     setIsMounted(true);
   }, []);
+
+  const renderAddress = () => {
+    return deliveryAddress
+      ? `${deliveryAddress.street}, 
+        ${BaseUtil.renderAddressName(
+          deliveryAddress.ward,
+          deliveryAddress.district,
+          deliveryAddress.province
+        )} `
+      : "";
+  };
+
+  return isMounted ? (
+    <div className="bg-white py-6 px-8 border-t-2 border-primary">
+      <div className="flex items-center space-x-2 text-lg text-primary leading-none">
+        <MapPin size={18} />
+        <p>Địa chỉ nhận hàng</p>
+      </div>
+      <div className="flex items-center space-x-10 pt-2">
+        <div className="flex space-x-2 font-bold">
+          <h4>{deliveryAddress?.name}</h4>
+          <h4>{BaseUtil.formatPhoneNUmber(deliveryAddress?.phone)}</h4>
+        </div>
+        <div className="flex items-center space-x-4">
+          <p className="">{renderAddress()}</p>
+          {deliveryAddress?.default && (
+            <div className="text-primary text-[10px] border border-primary px-1.5 py-0.5 relative overflow-hidden">
+              <span className="size-2 bg-primary absolute top-0 left-0 -translate-x-1 -translate-y-1 rotate-45"></span>
+              <span>Mặc định</span>
+            </div>
+          )}
+        </div>
+        <AddressListSelector />
+      </div>
+    </div>
+  ) : (
+    <DeliveryAddressPlaceHolder />
+  );
+}
+
+const DeliveryAddressPlaceHolder = () => {
   return (
     <div className="bg-white py-6 px-8 border-t-2 border-primary">
       <div className="flex items-center space-x-2 text-lg text-primary leading-none">
@@ -31,42 +70,16 @@ export function DeliveryAddress() {
       </div>
       <div className="flex items-center space-x-10 pt-2">
         <div className="flex space-x-2 font-bold">
-          {isMounted ? (
-            <h4>{address?.name}</h4>
-          ) : (
-            <Skeleton className="w-[88px] h-6 mt-2" />
-          )}
-          {isMounted ? (
-            <h4>{BaseUtil.formatPhoneNUmber(address?.phone)}</h4>
-          ) : (
-            <Skeleton className="w-[104px] h-6 my-2" />
-          )}
+          <Skeleton className="w-[88px] h-6 mt-2" />
+          <Skeleton className="w-[104px] h-6 my-2" />
         </div>
         <div className="flex items-center space-x-4">
-          {isMounted ? (
-            <p className="">
-              {`${address?.street}, ${BaseUtil.renderAddressName(
-                address?.ward,
-                address?.district,
-                address?.province
-              )} `}
-            </p>
-          ) : (
-            <Skeleton className="w-[326px] h-6 my-2" />
-          )}
-          <div className="text-primary text-[10px] border border-primary px-1.5 py-0.5 relative overflow-hidden">
-            <span className="size-2 bg-primary absolute top-0 left-0 -translate-x-1 -translate-y-1 rotate-45"></span>
-            <span>Mặc định</span>
-          </div>
+          <Skeleton className="w-[326px] h-6 my-2" />
         </div>
-        {isMounted ? (
-          <AddressListSelector setAddress={setAddress} />
-        ) : (
-          <div className="px-4">
-            <Skeleton className="w-14 h-6 my-2" />
-          </div>
-        )}
+        <div className="px-4">
+          <Skeleton className="w-16 h-6 my-2 ml-16" />
+        </div>
       </div>
     </div>
   );
-}
+};
