8235469a3e07a0af5ce8036778f93e4e365111a2
Van Truong <truongtran.lv2019@gmail.com>
2024-07-20 21:58:13 +0700

    update

--- src/apis/user-address.ts
+++ src/apis/user-address.ts
@@ -7,25 +7,31 @@ import { EndpointUtil } from "~/common/utility/endpoint.util";
 
 const userAddressApi = {
   createAddress: (payload: CreateAddressRequest) =>
-    httpClient.post<any>(EndpointUtil.NEST.ADDRESS.CREATE_ADDRESS, payload),
+    httpClient.post<any>(
+      EndpointUtil.NEST.ADDRESS.USER.CREATE_ADDRESS,
+      payload
+    ),
   updateAddress: (payload: UpdateAddressRequest) =>
-    httpClient.post<any>(EndpointUtil.NEST.ADDRESS.UPDATE_ADDRESS, payload),
+    httpClient.post<any>(
+      EndpointUtil.NEST.ADDRESS.USER.UPDATE_ADDRESS,
+      payload
+    ),
   deleteAddress: (email: string, addressId: number) =>
     httpClient.delete<any>(
-      EndpointUtil.NEST.ADDRESS.DELETE_ADDRESS +
+      EndpointUtil.NEST.ADDRESS.USER.DELETE_ADDRESS +
         `?email=${email}&addressId=${addressId}`
     ),
   getAllAddress: (email: string) =>
     httpClient.get<any>(
-      EndpointUtil.NEST.ADDRESS.GET_ALL_BY_USER_EMAIL + `/${email}`
+      EndpointUtil.NEST.ADDRESS.USER.GET_ALL_BY_USER_EMAIL + `/${email}`
     ),
   getDefaultAddress: (email: string) =>
     httpClient.get<any>(
-      EndpointUtil.NEST.ADDRESS.GET_DEFAULT_ADDRESS + `/${email}`
+      EndpointUtil.NEST.ADDRESS.USER.GET_DEFAULT_ADDRESS + `/${email}`
     ),
   setDefaultAddress: (email: string, addressId: number) =>
     httpClient.post<any>(
-      EndpointUtil.NEST.ADDRESS.UPDATE_DEFAULT_ADDRESS +
+      EndpointUtil.NEST.ADDRESS.USER.UPDATE_DEFAULT_ADDRESS +
         `?email=${email}&addressId=${addressId}`
     ),
 };
